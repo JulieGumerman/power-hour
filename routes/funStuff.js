6756ecb031b67c41cc0b93b86bf4d9dacd263fe6
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-03 14:20:10 -0600

    heroku deployment, here I come.

---
+++ routes/funStuff.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const rt = express.Router;
+const data = require("./funStuffModel");
+
+rt.get("/", (req, res) => {
+    data.get
+        .then
+})
---
+++ routes/funStuffModel.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        name: "Julie",
+        age: 37, 
+        food: "pizza"
+    },
+    {
+        name: "Julie2",
+        age: 37, 
+        food: "pizza"
+    },
+    {
+        name: "Julie",
+        age: 37, 
+        food: "pizza"
+    }
+]
+
+const getData = () => {
+    return data;
+}
+
+module.exports = getData;
